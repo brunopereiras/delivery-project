44df6e5ad81c7d5ab7a8d474bde6eefc83bbb732
Bruno Duarte <brunopereira23@gmail.com>
2018-05-21 22:35:56 -0300

    Adicionado o seed do tipo de usuário.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+UserType.create([
+    { name: 'Empresa' },
+    { name: 'Estabelecimento' },
+    { name: 'Cliente' }
+])
