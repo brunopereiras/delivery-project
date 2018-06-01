24639050d963648f4711fd823b678a1a54435f02
Bruno Duarte <brunopereira23@gmail.com>
2018-06-01 19:20:35 -0300

    Pequenas correções nas rotas e no estilo generico.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
-  
-  resources :products
-  resources :product_categories
-  resources :establishments
+
   root "hotsite#index"
-  
-  get '/login', to: 'user_session#new'
-  post '/login', to: 'user_session#create'
-  delete '/logout', to: 'user_session#destroy'
 
   get 'hotsite/index'
   get 'hotsite/about'
 
   resources :user_types
   resources :users
+
+  get '/login', to: 'user_session#new'
+  post '/login', to: 'user_session#create'
+  delete '/logout', to: 'user_session#destroy'
+
+  resources :establishments
+  resources :product_categories
+  resources :products
 
 end
